e50a1599b3bdff02b4dc88b9bb6420787c05157f
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-08-17 17:17:06 +0800

    取消input自动填充功能

--- lichkin-ui/src/main/resources/META-INF/resources/webjars/LichKin-UI/1.0.0/plugins/lichkin-plugin-core.js
+++ lichkin-ui/src/main/resources/META-INF/resources/webjars/LichKin-UI/1.0.0/plugins/lichkin-plugin-core.js
@@ -641,7 +641,7 @@ LK.UI('plugins', 'create', function(opts) {
   $plugin.data('initValue', options.value);
 
   // 创建UI控件存值对象
-  var $value = $((plugin == 'textbox' || plugin == 'datepicker' || plugin == 'timepicker' || plugin == 'numberspinner') ? '<input type="text"' + (plugin == 'datepicker' || plugin == 'timepicker' ? ' readonly="true"' : '') + ' />' : (plugin == 'textarea' ? '<textarea></textarea>' : '<input type="hidden" />')).appendTo($plugin);
+  var $value = $((plugin == 'textbox' || plugin == 'datepicker' || plugin == 'timepicker' || plugin == 'numberspinner') ? '<input type="text" autocomplete="off"' + (plugin == 'datepicker' || plugin == 'timepicker' ? ' readonly="true"' : '') + ' />' : (plugin == 'textarea' ? '<textarea></textarea>' : '<input type="hidden" />')).appendTo($plugin);
   $value.LKAddPluginClass(plugin, 'value');
   $value.attr('name', options.name);
   $value.data({
