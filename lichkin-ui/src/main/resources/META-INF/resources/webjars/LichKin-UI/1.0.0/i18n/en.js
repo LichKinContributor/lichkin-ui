edca03d472c4f15f36950160b08193a33cc235f5
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-21 16:39:57 +0800

    补充国际化配置
    Signed-off-by: China-zhuangxuxin <zhuangxuxin@hotmail.com>

---
+++ lichkin-ui/src/main/resources/META-INF/resources/webjars/LichKin-UI/1.0.0/i18n/en.js
@@ -0,0 +1,102 @@
+;
+/**
+ * Internationalization implementation
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+$.LKExtendI18N({
+  'timeout' : 'Request timeout, please try again later!',
+  'ajaxError' : 'An exception occurred on the server request',
+
+  /** 控件 */
+  'click' : 'click',
+
+  'save' : 'save',
+  'ok' : 'ok',
+  'cancel' : 'cancel',
+  'clear' : 'clear',
+  'close' : 'close',
+
+  'tip' : 'tip',
+  'warning' : 'warning',
+
+  'dropdown' : 'show/hide',
+
+  'datepicker' : 'show/hide',
+
+  'timepicker' : 'show/hide',
+  'hour' : 'hour',
+  'minutes' : 'minutes',
+
+  'plus' : 'plus',
+  'minus' : 'minus',
+
+  'page' : 'page',
+  'folder' : 'folder',
+
+  'selector' : 'show/hide',
+
+  'selector-employee-title' : 'employee selector',
+  'department' : 'department',
+  'selector-employee-dlg' : {
+    'grid' : {
+      'source' : 'source',
+      'target' : 'target',
+      'columns' : {
+        'userName' : 'userName',
+        'gender' : 'gender',
+        'jobNumber' : 'jobNumber',
+        'department' : 'department'
+      }
+    }
+  },
+
+  'selector-product-title' : 'product selector',
+  'productCategory' : 'productCategory',
+  'selector-product-dlg' : {
+    'grid' : {
+      'source' : 'source',
+      'target' : 'target',
+      'columns' : {
+        'productName' : 'productName',
+        'productCode' : 'productCode',
+        'unit' : 'unit',
+        'barcode' : 'barcode',
+        'retailPrice' : 'retailPrice',
+      }
+    }
+  },
+
+  'cropper' : 'cropper',
+  'cropper-no-image' : 'no image',
+  'upload' : 'upload',
+  'zoomIn' : 'zoomIn',
+  'zoomOut' : 'zoomOut',
+  'reverseX' : 'reverseX',
+  'reverseY' : 'reverseY',
+  'noSelectImage' : 'Please select a picture first',
+
+  'datagrid-statistics' : 'from {from} to {to}, total: {total}.',
+  'go-first' : 'first',
+  'go-previous' : 'previous',
+  'go-next' : 'next',
+  'go-last' : 'last',
+  'page-prefix' : 'page',
+  'page-suffix' : ', total: {totalPages}.',
+  'singleSelect' : 'Only one data item can be selected',
+  'noSelect' : 'Please select one data first',
+  'add' : 'add',
+  'remove' : 'remove',
+  'edit' : 'edit',
+  'view' : 'view',
+  'search' : 'search',
+  'reset' : 'reset',
+  'release' : 'release',
+  'submit' : 'submit',
+  'lock' : 'lock',
+  'unlock' : 'unlock',
+  'set' : 'set',
+  'cut' : 'cut',
+
+  'No more datas' : 'No more datas',
+
+});
