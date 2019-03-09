c3f5bf37c9594f32d08ed1e53e8f966495a83f60
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2019-03-09 17:04:04 +0800

    launcher cmd完善

--- lichkin-ui-launcher/run.bat
+++ lichkin-ui-launcher/run.bat
@@ -1,2 +1,2 @@
-cd target\lichkin-projects-ui-launcher-1.0.0.RELEASE
-java org.springframework.boot.loader.JarLauncher --spring.profiles.active=ui,localhost --server.servlet.context-path=/ui --server.port=33333 --log.tag=LichKin-UI --log.level.system=debug --log.level.org=info --log.level.net=info --lichkin.locale.default=zh_CN --lichkin.locale.implemented=zh_CN --lichkin.system.tag=LichKin-UI --lichkin.system.name=UI平台 --lichkin.system.debug=false --lichkin.web.debug=false --lichkin.web.admin.debug=false --lichkin.web.compress=true
+cd target\lichkin-ui-launcher-1.0.0.RELEASE
+java org.springframework.boot.loader.JarLauncher --spring.profiles.active=ui,localhost --server.servlet.context-path=/ui --server.port=33333 --log.tag=LichKin-UI --lichkin.system.tag=LichKin-UI --lichkin.system.name=UI平台 --lichkin.system.debug=false --lichkin.web.debug=false --lichkin.web.admin.debug=false --lichkin.web.compress=true
--- lichkin-ui-launcher/run.sh
+++ lichkin-ui-launcher/run.sh
@@ -1,6 +1,6 @@
 ps -ef | grep LichKin-UI | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
 cd /opt/runnings/ui/
-nohup java org.springframework.boot.loader.JarLauncher --spring.profiles.active=ui,localhost --server.servlet.context-path=/ui --server.port=33333 --log.tag=LichKin-UI --log.level.system=debug --log.level.org=info --log.level.net=info --lichkin.locale.default=zh_CN --lichkin.locale.implemented=zh_CN --lichkin.system.tag=LichKin-UI --lichkin.system.name=UI平台 --lichkin.system.debug=false --lichkin.web.debug=false --lichkin.web.admin.debug=false --lichkin.web.compress=true >/dev/null 2>&1 &
+nohup java org.springframework.boot.loader.JarLauncher --spring.profiles.active=ui,localhost --server.servlet.context-path=/ui --server.port=33333 --log.tag=LichKin-UI --lichkin.system.tag=LichKin-UI --lichkin.system.name=UI平台 --lichkin.system.debug=false --lichkin.web.debug=false --lichkin.web.admin.debug=false --lichkin.web.compress=true >/dev/null 2>&1 &
 
 tail -f /lichkin-logs/LichKin-UI.debug.log
